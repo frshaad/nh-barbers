9fadf6d99c3cd22a5b97ef8fdda8f37cc94b607b
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-03-15 21:17:35 +0330

    implement Filters component for shop type and service filtering

--- src/app/barbers/_components/filters.tsx
+++ src/app/barbers/_components/filters.tsx
@@ -1,14 +1,80 @@
+'use client';
+
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Label } from '@/components/ui/label';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export default function Filters() {
   return (
     <Card className="flex-1">
       <CardHeader>
-        <CardTitle>فیلترها</CardTitle>
+        <CardTitle className="text-xl">فیلترها</CardTitle>
       </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
+      <CardContent className="space-y-6">
+        <TypeFilter />
+        <ServicesFilter />
       </CardContent>
     </Card>
+  );
+}
+
+function TypeFilter() {
+  return (
+    <div className="space-y-2">
+      <Label htmlFor="shop-type" className="text-base">
+        نوع فروشگاه
+      </Label>
+      <Tabs
+        defaultValue="all"
+        className="w-full"
+        dir="rtl"
+        onValueChange={(v) => console.log(v)}
+        id="shop-type"
+      >
+        <TabsList className="w-full">
+          <TabsTrigger value="all">همه</TabsTrigger>
+          <TabsTrigger value="shop">فقط فروشگاه‌ها</TabsTrigger>
+          <TabsTrigger value="noshop">آرایشگران فردی</TabsTrigger>
+        </TabsList>
+      </Tabs>
+    </div>
+  );
+}
+
+function ServicesFilter() {
+  return (
+    <div className="space-y-2">
+      <Label className="text-base">سرویس‌ها</Label>
+      <div className="space-y-1.5">
+        <div className="flex items-center space-x-2">
+          <Checkbox id="terms" className="cursor-pointer" />
+          <label
+            htmlFor="terms"
+            className="cursor-pointer text-sm leading-none font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            سرویس اول
+          </label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <Checkbox id="termss" className="cursor-pointer" />
+          <label
+            htmlFor="termss"
+            className="cursor-pointer text-sm leading-none font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            سرویس اول
+          </label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <Checkbox id="termas" className="cursor-pointer" />
+          <label
+            htmlFor="termas"
+            className="cursor-pointer text-sm leading-none font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            سرویس اول
+          </label>
+        </div>
+      </div>
+    </div>
   );
 }
