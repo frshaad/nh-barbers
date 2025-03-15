5f9392e836de316d18a74a521b025c8ac37310b1
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-03-16 00:35:02 +0330

    refactor TypeFilter to use query state for shop type selection

--- src/app/barbers/_components/type-filter.tsx
+++ src/app/barbers/_components/type-filter.tsx
@@ -1,25 +1,28 @@
 'use client';
 
+import { useQueryState } from 'nuqs';
 import { Label } from '@/components/ui/label';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export default function TypeFilter() {
+  const [isShop, setIsShop] = useQueryState('is_shop', { defaultValue: 'all' });
+
   return (
     <div className="space-y-2">
       <Label htmlFor="shop-type" className="text-base">
         نوع فروشگاه
       </Label>
       <Tabs
-        defaultValue="all"
+        defaultValue={isShop}
         className="w-full"
         dir="rtl"
-        onValueChange={(v) => console.log(v)}
+        onValueChange={(value) => setIsShop(value)}
         id="shop-type"
       >
         <TabsList className="w-full">
           <TabsTrigger value="all">همه</TabsTrigger>
-          <TabsTrigger value="shop">فقط فروشگاه‌ها</TabsTrigger>
-          <TabsTrigger value="noshop">آرایشگران فردی</TabsTrigger>
+          <TabsTrigger value="true">فقط فروشگاه‌ها</TabsTrigger>
+          <TabsTrigger value="false">آرایشگران فردی</TabsTrigger>
         </TabsList>
       </Tabs>
     </div>
