33069c9bb500a4527f0fb260123d58181ff25ae8
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-03-15 20:58:05 +0330

    add Badge component and update BarberCard to include service badges

--- src/app/barbers/_components/barber-card.tsx
+++ src/app/barbers/_components/barber-card.tsx
@@ -1,8 +1,8 @@
 import RatingStars from '@/app/barbers/_components/rating-stars';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Badge } from '@/components/ui/badge';
 import {
   Card,
-  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
@@ -11,24 +11,31 @@ import {
 
 export default function BarberCard() {
   return (
-    <Card className="flex-row items-center">
-      <CardHeader className="flex-row gap-2.5">
-        <Avatar className="size-14">
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-        <div className="space-y-1">
-          <CardTitle>محسن صادقی</CardTitle>
-          <CardDescription>
-            خیابان امام، کوچه شهید درویشی، جنب میوه فروشی
-          </CardDescription>
+    <Card>
+      <CardHeader className="flex-row justify-between gap-3">
+        <div className="flex items-center gap-2.5">
+          <Avatar className="size-14">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <div className="space-y-2">
+            <CardTitle>محسن صادقی</CardTitle>
+            <CardDescription>
+              خیابان امام، کوچه شهید درویشی، جنب میوه فروشی خیابان امام، کوچه
+              شهید درویشی، جنب میوه فروشی
+            </CardDescription>
+          </div>
         </div>
-      </CardHeader>
-      <CardContent>
-        <p>سرویس ها</p>
-      </CardContent>
-      <CardFooter>
         <RatingStars rating={2.5} />
+      </CardHeader>
+
+      <CardFooter className="flex-wrap gap-1">
+        <Badge variant="secondary">سرویس</Badge>
+        <Badge variant="secondary">سرویس</Badge>
+        <Badge variant="secondary">سرویس</Badge>
+        <Badge variant="secondary">سرویس</Badge>
+        <Badge variant="secondary">سرویس</Badge>
+        <Badge variant="secondary">سرویس</Badge>
       </CardFooter>
     </Card>
   );
