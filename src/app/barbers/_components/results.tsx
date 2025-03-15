cf9bc01339af6218aec9f0f1dffeacaca9b0b78a
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-03-15 20:40:18 +0330

    update Results component to set fixed height and enable vertical scrolling for better display of BarberCard items

--- src/app/barbers/_components/results.tsx
+++ src/app/barbers/_components/results.tsx
@@ -2,7 +2,11 @@ import BarberCard from '@/app/barbers/_components/barber-card';
 
 export default function Results() {
   return (
-    <div className="flex-[2] space-y-3">
+    <div className="h-[600px] flex-[2] space-y-3 overflow-y-scroll">
+      <BarberCard />
+      <BarberCard />
+      <BarberCard />
+      <BarberCard />
       <BarberCard />
       <BarberCard />
       <BarberCard />
