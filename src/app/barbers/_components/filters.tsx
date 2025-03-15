f7c5eccfbdfafa1f49c8aa8c991a64be3526ee83
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-03-15 20:19:30 +0330

    add Filters component for displaying filter options in Barbers page

---
+++ src/app/barbers/_components/filters.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+export default function Filters() {
+  return (
+    <Card className="flex-1">
+      <CardHeader>
+        <CardTitle>فیلترها</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+    </Card>
+  );
+}
