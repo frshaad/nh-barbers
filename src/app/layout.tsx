a018fe91cb23dd406e7b0e7e18a5144c129be05f
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-03-15 17:24:24 +0330

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next';
+import { ThemeProvider } from '@/components/theme-provider';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Next.js Starter App',
+  description: 'A basic starter for Next.js',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className="h-screen w-screen">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          disableTransitionOnChange
+          enableSystem
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
